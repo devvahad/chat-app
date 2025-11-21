e6c56c005fd1cb9002e587eb2478aef718465173
devvahad <ahadanis.devv@gmail.com>
2025-11-21 10:39:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
 const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-// const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+}));
+
 app.use(express.json())
 app.use(cookiesParser())
 
-const PORT = process.env.PORT || 8080
+const PORT = Number(process.env.PORT) || 5000;
 
 app.get('/',(request,response)=>{
     response.json({
